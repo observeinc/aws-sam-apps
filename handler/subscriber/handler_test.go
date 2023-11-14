c68faaebe93b6b26f5cde68d77f64aa6ae145bba
João Taveira Araújo <joao@observeinc.com>
2023-11-14 13:25:22 -0800

    feat(subscriber): add subscription diff logic (#80)
    First pass at fleshing out the `HandleSubscribe` endpoint. This is
    unlikely to have all the safeguards we need, but for now:
    
    - we only manage subscription filters which have our filter name as a
      prefix.
    - if there are already 2 filters, we do nothing
    
    The diffing function is broken out so that we can test it without
    involving AWS API stubs.

--- handler/subscriber/handler_test.go
+++ handler/subscriber/handler_test.go
@@ -7,8 +7,10 @@ import (
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 
 	"github.com/observeinc/aws-sam-testing/handler/handlertest"
 	"github.com/observeinc/aws-sam-testing/handler/subscriber"
@@ -137,6 +139,123 @@ func TestHandleDiscovery(t *testing.T) {
 			}
 
 			if diff := cmp.Diff(tt.ExpectResponse, resp); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func TestSubscriptionFilterDiff(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		Configure       types.SubscriptionFilter
+		Existing        []types.SubscriptionFilter
+		ExpectedActions []any
+	}{
+		{
+			/*
+				In the absence of a destination ARN, we delete all subscription
+				filters that contain our filter name as a prefix.
+			*/
+			Configure: types.SubscriptionFilter{
+				FilterName: aws.String("observe"),
+			},
+			Existing: []types.SubscriptionFilter{
+				{
+					FilterName: aws.String("foo"),
+				},
+				{
+					FilterName: aws.String("observe-logs-subscription"),
+				},
+			},
+			ExpectedActions: []any{
+				&cloudwatchlogs.DeleteSubscriptionFilterInput{
+					FilterName: aws.String("observe-logs-subscription"),
+				},
+			},
+		},
+		{
+			Configure: types.SubscriptionFilter{
+				FilterName:     aws.String("observe"),
+				DestinationArn: aws.String("arn:aws:lambda:us-west-2:123456789012:function:example"),
+			},
+			Existing: []types.SubscriptionFilter{
+				{
+					FilterName: aws.String("foo"),
+				},
+				{
+					FilterName: aws.String("observe-logs-subscription"),
+				},
+			},
+			ExpectedActions: []any{
+				&cloudwatchlogs.DeleteSubscriptionFilterInput{
+					FilterName: aws.String("observe-logs-subscription"),
+				},
+				&cloudwatchlogs.PutSubscriptionFilterInput{
+					FilterName:     aws.String("observe"),
+					FilterPattern:  aws.String(""),
+					DestinationArn: aws.String("arn:aws:lambda:us-west-2:123456789012:function:example"),
+				},
+			},
+		},
+		{
+			/*
+				Do nothing if we exceed the two subscription filter limit
+			*/
+			Configure: types.SubscriptionFilter{
+				FilterName:     aws.String("observe"),
+				DestinationArn: aws.String("arn:aws:lambda:us-west-2:123456789012:function:example"),
+			},
+			Existing: []types.SubscriptionFilter{},
+			ExpectedActions: []any{
+				&cloudwatchlogs.PutSubscriptionFilterInput{
+					FilterName:     aws.String("observe"),
+					FilterPattern:  aws.String(""),
+					DestinationArn: aws.String("arn:aws:lambda:us-west-2:123456789012:function:example"),
+				},
+			},
+		},
+		{
+			Configure: types.SubscriptionFilter{
+				FilterName:     aws.String("observe"),
+				DestinationArn: aws.String("arn:aws:lambda:us-west-2:123456789012:function:example"),
+			},
+			Existing: []types.SubscriptionFilter{
+				{
+					FilterName: aws.String("foo"),
+				},
+				{
+					FilterName: aws.String("bar"),
+				},
+			},
+			// no expected actions
+		},
+	}
+
+	for i, tt := range testcases {
+		tt := tt
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+			s, err := subscriber.New(
+				&subscriber.Config{
+					CloudWatchLogsClient: &handlertest.CloudWatchLogsClient{},
+					Queue:                &MockQueue{},
+					FilterName:           aws.ToString(tt.Configure.FilterName),
+					DestinationARN:       aws.ToString(tt.Configure.DestinationArn),
+					RoleARN:              aws.ToString(tt.Configure.RoleArn),
+				})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			output := s.SubscriptionFilterDiff(tt.Existing)
+
+			opts := cmpopts.IgnoreUnexported(
+				cloudwatchlogs.PutSubscriptionFilterInput{},
+				cloudwatchlogs.DeleteSubscriptionFilterInput{},
+			)
+			if diff := cmp.Diff(output, tt.ExpectedActions, opts); diff != "" {
 				t.Error(diff)
 			}
 		})
