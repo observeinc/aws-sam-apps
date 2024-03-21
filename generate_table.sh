0459edb3648390854436745bd4abb2c3a9d6d22b
João Taveira Araújo <joao@observeinc.com>
2024-03-21 09:14:25 -0700

    feat: rename collection stack
    For parity with terraform, let's rename the collection stack to simply
    "stack". This has the disadvantage of clashing with cloudformation
    nomenclature ("stack" is the instantiation of a template), but it
    matches how people refer to artefact ("install the Observe
    cloudformation stack").

--- generate_table.sh
+++ generate_table.sh
@@ -4,7 +4,7 @@
 regions=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text | tr '\t' '\n')
 
 # Initialize the table with headers
-echo "| Collection | Config | Forwarder |"
+echo "| Stack | Config | Forwarder |"
 echo "|------------|--------|-----------|"
 
 # Generate rows for each region
@@ -16,10 +16,10 @@ for region in $regions; do
     badge_md="![Static Badge](https://img.shields.io/badge/$normalized_region-latest-blue?logo=amazonaws)"
     
     # Create CloudFormation console links
-    collection_link="https://$region.console.aws.amazon.com/cloudformation/home?region=$region#/stacks/create/review?templateURL=https://observeinc-$region.s3.amazonaws.com/apps/collection/latest/packaged.yaml"
+    stack_link="https://$region.console.aws.amazon.com/cloudformation/home?region=$region#/stacks/create/review?templateURL=https://observeinc-$region.s3.amazonaws.com/apps/stack/latest/packaged.yaml"
     config_link="https://$region.console.aws.amazon.com/cloudformation/home?region=$region#/stacks/create/review?templateURL=https://observeinc-$region.s3.amazonaws.com/apps/config/latest/packaged.yaml"
     forwarder_link="https://$region.console.aws.amazon.com/cloudformation/home?region=$region#/stacks/create/review?templateURL=https://observeinc-$region.s3.amazonaws.com/apps/forwarder/latest/packaged.yaml"
     
     # Generate table row with shield badges linking to CloudFormation console
-    echo "| [$badge_md]($collection_link) | [$badge_md]($config_link) | [$badge_md]($forwarder_link) |"
+    echo "| [$badge_md]($stack_link) | [$badge_md]($config_link) | [$badge_md]($forwarder_link) |"
 done
