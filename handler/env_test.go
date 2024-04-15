0445a0378d4d546452590d15ef94b8855cd3a716
João Taveira Araújo <joao@observeinc.com>
2024-04-15 15:06:43 -0700

    fix: export defaults to environment variables (#221)
    Setting a default in our env struct currently only affects the value
    being read into the struct. It is generally useful to have that value
    exporting back to the environment, in the case where other libraries
    source their configuration from the environment directly.
    
    An existing usecase is where we modify the default for
    `AWS_MAX_ATTEMPTS`. The AWS SDK reads this environment variable
    directly when configuring the client.

---
+++ handler/env_test.go
@@ -0,0 +1,30 @@
+package handler_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/observeinc/aws-sam-apps/handler"
+)
+
+func TestEnv(t *testing.T) {
+	env := struct {
+		Embedded struct {
+			Test         string `env:"TEST,default=hello"`
+			ListOfValues string `env:"LISTOFVALUES"`
+		} `env:", prefix=T_"`
+	}{}
+
+	if v := os.Getenv("T_TEST"); v != "" {
+		t.Fatal("found unexpected environment variable")
+	}
+
+	if err := handler.ProcessEnv(context.Background(), &env); err != nil {
+		t.Fatal(err)
+	}
+
+	if v := os.Getenv("T_TEST"); v != "hello" {
+		t.Fatal("default was not exported back to environment")
+	}
+}
