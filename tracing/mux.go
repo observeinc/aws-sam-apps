bf76ca1d2c9a31cb164522e71a75808b34444e91
João Taveira Araújo <joao@observeinc.com>
2024-05-31 14:27:59 -0700

    fix(tracing): set flusher in otellambda instrumentation
    This ensures we flush data prior to lambda freezing.

--- tracing/mux.go
+++ tracing/mux.go
@@ -6,15 +6,17 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda"
 	"go.opentelemetry.io/otel/attribute"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
 )
 
 var payloadKey = attribute.Key("payload")
 
-func NewLambdaHandler(handler lambda.Handler, tp trace.TracerProvider) lambda.Handler {
+func NewLambdaHandler(handler lambda.Handler, tp *sdktrace.TracerProvider) lambda.Handler {
 	return otellambda.WrapHandler(
 		&LambdaHandler{handler},
 		otellambda.WithTracerProvider(tp),
+		otellambda.WithFlusher(tp),
 	)
 }
 
