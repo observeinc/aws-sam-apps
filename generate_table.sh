a94d801f4dcbd3d802935fd0865ad33c20d5c6a4
Justin Daines <168476932+obs-gh-justindaines@users.noreply.github.com>
2026-04-11 05:06:02 -0400

    feat: add support for eu-central-2 and af-south-1 regions

--- generate_table.sh
+++ generate_table.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Fetch available AWS regions
-regions=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text | tr '\t' '\n')
+# Read supported regions from variables.mk
+# Extract AWS_REGIONS from variables.mk (keep original order)
+regions=$(awk '/^AWS_REGIONS[[:space:]]*:=/ {flag=1; next} /^$/ {flag=0} flag {gsub(/\\/, ""); gsub(/\)/, ""); gsub(/^[[:space:]]+|[[:space:]]+$/, ""); if (length > 0) print}' variables.mk)
 
 # Initialize the table with headers
 echo "| Stack | Config | Forwarder |"
@@ -11,15 +12,15 @@ echo "|------------|--------|-----------|"
 for region in $regions; do
     # Normalize region name for badge (replace '-' with '_')
     normalized_region=$(echo $region | tr '-' '_')
-    
+
     # Create shield badge markdown
     badge_md="![Static Badge](https://img.shields.io/badge/$normalized_region-latest-blue?logo=amazonaws)"
-    
+
     # Create CloudFormation console links
     stack_link="https://$region.console.aws.amazon.com/cloudformation/home?region=$region#/stacks/create/review?templateURL=https://observeinc-$region.s3.amazonaws.com/aws-sam-apps/latest/stack.yaml"
     config_link="https://$region.console.aws.amazon.com/cloudformation/home?region=$region#/stacks/create/review?templateURL=https://observeinc-$region.s3.amazonaws.com/aws-sam-apps/latest/config.yaml"
     forwarder_link="https://$region.console.aws.amazon.com/cloudformation/home?region=$region#/stacks/create/review?templateURL=https://observeinc-$region.s3.amazonaws.com/aws-sam-apps/latest/forwarder.yaml"
-    
+
     # Generate table row with shield badges linking to CloudFormation console
     echo "| [$badge_md]($stack_link) | [$badge_md]($config_link) | [$badge_md]($forwarder_link) |"
 done
